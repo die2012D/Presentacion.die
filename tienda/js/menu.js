3940bc67af083b28b6dffaa3dd4a38f4fb239db2
die2012D <36577261+die2012D@users.noreply.github.com>
2022-11-08 13:39:33 -0500

    tienda

---
+++ tienda/js/menu.js
@@ -0,0 +1,5 @@
+let btnMenu = document.getElementById('btn-menu');
+let mainnav = document.getElementById('main-nav');
+btnMenu.addEventListener('click', function(){
+    mainnav.classList.toggle('mostrar');
+});
